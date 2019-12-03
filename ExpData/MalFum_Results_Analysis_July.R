18e713ac659be311783c2ac9912be1062fc6bd59
HAHerrmann <hherrmann2@gmail.com>
2019-12-03 12:07:32 +0000

    Fig Legend Update

--- ExpData/MalFum_Results_Analysis_July.R
+++ ExpData/MalFum_Results_Analysis_July.R
@@ -37,7 +37,7 @@ mal_fum <- function(con,geno,temps){
 ma = max(conc_mal,na.rm = TRUE)
 mi = 0
 
-malfum_plot <- function(genotype,concen,temps,title){
+malfum_plot <- function(genotype,concen,temps,title,a){
   amm = mal_fum(concen,genotype,temps)
   data_gen = matrix(rep(NA,8*6),nrow=8,ncol=6)
   colnames(data_gen)=c(5,10,15,20,25,30)
@@ -46,31 +46,31 @@ malfum_plot <- function(genotype,concen,temps,title){
   for (i in seq(2,7)){
     data_gen[i,] = data_gen[i-1,] + (data_gen[8,]-data_gen[1,])/7
   }
-  View(data_gen)
-  plot(temps,amm$metabo_BOD,type="o", col="blue",main=title,
+  #View(data_gen)
+  plot(temps,amm$metabo_BOD,type="o", col="blue",main=title,cex=1.5,pch=19,
        xlab='',ylab='',ylim=c(mi,ma),cex.main=1.5,col.axis=cl,col.lab=cl,col.main=cl)
   errbar(temps, amm$metabo_BOD, amm$metabo_BOD+amm$se_BOD, amm$metabo_BOD-amm$se_BOD,
          add=TRUE,col='blue')
-  points(temps,amm$metabo_EOD,type="o", col="green")
-  title(ylab=expression(paste(mu,'mol/(gDW)')), line=2.2, cex.lab=1.5,col.lab=cl)
+  points(temps,amm$metabo_EOD,type="o",pch=19, col="green",cex=1.5)
+  title(ylab=expression(paste('mmol/(gDW)')), line=2.2, cex.lab=1.5,col.lab=cl)
   title(xlab=expression(~degree~C), line=2.2, cex.lab=1.5,col.lab=cl)
   errbar(temps, amm$metabo_EOD, amm$metabo_EOD+amm$se_EOD, amm$metabo_EOD-amm$se_EOD,
          add=TRUE,col='green')
-  legend("top",legend=c('BOD','EOD'),col=c('blue','green'),pch=c('o','o'),bty = "n")
+  legend("top",legend=c('BOD','EOD'),col=c('blue','green'),pch=c(19,19),cex=1.5,bty = "n")
+  legend("topleft", a, bty="n",cex=1.5) 
 }
 
 
 cl = 'black'
 par(mfrow=c(2,3),fg=cl) #,bg=NA,fg='white'
 
-malfum_plot('col0',conc_mal,temps,'Col-0 Malate')
-malfum_plot('fum2',conc_mal,temps,'Fum2.2 Malate')
-malfum_plot('c24',conc_mal,temps,'C24 Malate')
+malfum_plot('col0',conc_mal,temps,expression('Col-0 Malate'),"(A)")
+malfum_plot('fum2',conc_mal,temps,expression(paste(italic('fum2.2'),' Malate')),"(B)")
+malfum_plot('c24',conc_mal,temps,expression('C24 Malate'),"(C)")
 
-malfum_plot('col0',conc_fum,temps,'Col-0 Fumarate')
-malfum_plot('fum2',conc_fum,temps,'Fum2.2 Fumarate')
-malfum_plot('c24',conc_fum,temps,'C24 Fumarate')
-
+malfum_plot('col0',conc_fum,temps,expression('Col-0 Fumarate'),"(D)")
+malfum_plot('fum2',conc_fum,temps,expression(paste(italic('fum2.2'),' Fumarate')),"(E)")
+malfum_plot('c24',conc_fum,temps,expression('C24 Fumarate'),"(F)")
 #dev.copy(png,'MalFum.png')
 #dev.off()
 
